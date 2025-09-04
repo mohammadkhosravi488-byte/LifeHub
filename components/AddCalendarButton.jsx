dd07be5dd5bfe846287d5812e32a3d094549139e
mohammadkhosravi488-byte <mohammad.khosravi488@gmail.com>
2025-09-04 20:08:59 +1000

    Add missing AddCalendarButton component to fix build

---
+++ components/AddCalendarButton.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function AddCalendarButton({ onClick }) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className="h-8 px-4 rounded-[16px] border border-[var(--outline-neutral)] bg-white text-sm font-semibold"
+      title="Create a new calendar"
+    >
+      Add
+    </button>
+  );
+}
