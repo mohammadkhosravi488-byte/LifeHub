8e4ecae66f873cca4556d641c3e91afadfce8677
mohammadkhosravi488-byte <mohammad.khosravi488@gmail.com>
2025-09-14 19:24:30 +1000

    fix: lock dark mode to class + provider

--- components/Providers.jsx
+++ components/Providers.jsx
@@ -1,10 +1,13 @@
 "use client";
-
 import { ThemeProvider } from "next-themes";
 
 export default function Providers({ children }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider
+      attribute="class"     // adds/removes `class="dark"`
+      defaultTheme="light"  // start in light unless user changed it
+      enableSystem={false}  // disable OS/system theme
+    >
       {children}
     </ThemeProvider>
   );
